f3f11eefd8f524f4f262742965ff57c41d0f3db5
SpottedBush <vincenttardieux22@gmail.com>
2023-06-01 01:37:52 +0200

    fix(training.py):

--- training.py
+++ training.py
@@ -92,27 +92,28 @@ f = open(os.path.join('trained_models', 'results.txt'), "w+")
 num_epochs = 10
 for epoch in range(num_epochs):
     total_loss = 0
-    total_samples = 0
     for x, edge_index, y in dataloader:
         optimizer.zero_grad()
         x_padded = pad_sequence(x, batch_first=True)
+        edge_index_padded = pad_sequence(edge_index, batch_first=True)
         edge_index_list = []
         max_index = x_padded.size(1) - 1  # Initialize the maximum index as the last index of x_padded
         for e in edge_index:
             edge_index_list.append(e + max_index)  # Add the maximum index to adjust the indices
             max_index += x_padded.size(1)  # Update the maximum index
         edge_index_padded = torch.cat(edge_index_list, dim=1)
-        num_nodes = max_index + 1  # Update the number of nodes based on the adjusted indices
+
+        num_nodes = max_index.item() + 1  # Update the number of nodes based on the adjusted indices
         adj_matrix = torch.zeros(num_nodes, num_nodes, dtype=torch.float32)  # Adjust the size of adj_matrix
         for i, j in edge_index_padded.t():
             adj_matrix[i, j] = 1.0
+
         output = model(x_padded, edge_index_padded)  # Update the model forward call
         loss = criterion(output, y)
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
-        total_samples += y.size(0)
-    avg_loss = total_loss / total_samples
+    avg_loss = total_loss / len(dataloader)
     f.write(f"Epoch {epoch + 1} - Loss: {avg_loss}\n")
 
 print("\n\n------ Finished training, starting the evaluation ------")
@@ -124,16 +125,19 @@ total_samples = 0
 with torch.no_grad():
     for x, edge_index, y in dataloader:
         x_padded = pad_sequence(x, batch_first=True)
+        edge_index_padded = pad_sequence(edge_index, batch_first=True)
         edge_index_list = []
         max_index = x_padded.size(1) - 1  # Initialize the maximum index as the last index of x_padded
         for e in edge_index:
             edge_index_list.append(e + max_index)  # Add the maximum index to adjust the indices
             max_index += x_padded.size(1)  # Update the maximum index
         edge_index_padded = torch.cat(edge_index_list, dim=1)
-        num_nodes = max_index + 1  # Update the number of nodes based on the adjusted indices
+
+        num_nodes = max_index.item() + 1  # Update the number of nodes based on the adjusted indices
         adj_matrix = torch.zeros(num_nodes, num_nodes, dtype=torch.float32)  # Adjust the size of adj_matrix
         for i, j in edge_index_padded.t():
             adj_matrix[i, j] = 1.0
+
         output = model(x_padded, edge_index_padded)  # Update the model forward call
         predicted_labels = output.argmax(dim=1)
         total_correct += (predicted_labels == y).sum().item()
