5b3b897e26bad74b86c6c4736f2b7450702dc167
SpottedBush <vincenttardieux22@gmail.com>
2023-06-01 01:24:13 +0200

    fix(training.py): removed edge features

--- training.py
+++ training.py
@@ -17,10 +17,10 @@ class ChessGNN(nn.Module):
         self.conv2 = GCNConv(hidden_channels, hidden_channels)
         self.fc = nn.Linear(hidden_channels, num_classes)
 
-    def forward(self, x, edge_index, edge_attr):
-        x = self.conv1(x, edge_index, edge_attr)
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
         x = F.relu(x)
-        x = self.conv2(x, edge_index, edge_attr)
+        x = self.conv2(x, edge_index)
         x = F.relu(x)
         x = global_max_pool(x, torch.zeros(x.size(0), dtype=torch.long), x.size(0))
         x = self.fc(x)
@@ -66,9 +66,8 @@ class ChessDataset(Dataset):
     def __getitem__(self, index):
         x = torch.tensor(self.x_arr[index], dtype=torch.float32)
         edge_index = torch.tensor(self.tensors[index], dtype=torch.long)
-        edge_attr = torch.ones(edge_index.size(1), 1)
         y = torch.tensor(self.y_arr[index])
-        return x, edge_index, edge_attr, y
+        return x, edge_index, y
 
 
 file_path = os.path.join('Sets', 'training_set')
@@ -93,28 +92,27 @@ f = open(os.path.join('trained_models', 'results.txt'), "w+")
 num_epochs = 10
 for epoch in range(num_epochs):
     total_loss = 0
-    for x, edge_index, edge_attr, y in dataloader:
+    total_samples = 0
+    for x, edge_index, y in dataloader:
         optimizer.zero_grad()
         x_padded = pad_sequence(x, batch_first=True)
-        edge_attr_padded = pad_sequence(edge_attr, batch_first=True)
         edge_index_list = []
         max_index = x_padded.size(1) - 1  # Initialize the maximum index as the last index of x_padded
         for e in edge_index:
             edge_index_list.append(e + max_index)  # Add the maximum index to adjust the indices
             max_index += x_padded.size(1)  # Update the maximum index
         edge_index_padded = torch.cat(edge_index_list, dim=1)
-        edge_attr_padded = edge_attr_padded.reshape(edge_index_padded.size(1), 1)  # Reshape to (num_edges, 1)
         num_nodes = max_index + 1  # Update the number of nodes based on the adjusted indices
         adj_matrix = torch.zeros(num_nodes, num_nodes, dtype=torch.float32)  # Adjust the size of adj_matrix
         for i, j in edge_index_padded.t():
             adj_matrix[i, j] = 1.0
-        print("edge_index shape: ", edge_index_padded.size(1), "\nedge_attr shape: ", edge_attr_padded.size())
-        output = model(x_padded, edge_index_padded, edge_attr_padded)  # Update the model forward call
+        output = model(x_padded, edge_index_padded)  # Update the model forward call
         loss = criterion(output, y)
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
-    avg_loss = total_loss / len(dataloader)
+        total_samples += y.size(0)
+    avg_loss = total_loss / total_samples
     f.write(f"Epoch {epoch + 1} - Loss: {avg_loss}\n")
 
 print("\n\n------ Finished training, starting the evaluation ------")
@@ -124,21 +122,19 @@ model.eval()
 total_correct = 0
 total_samples = 0
 with torch.no_grad():
-    for x, edge_index, edge_attr, y in dataloader:
+    for x, edge_index, y in dataloader:
         x_padded = pad_sequence(x, batch_first=True)
-        edge_attr_padded = pad_sequence(edge_attr, batch_first=True)
         edge_index_list = []
         max_index = x_padded.size(1) - 1  # Initialize the maximum index as the last index of x_padded
         for e in edge_index:
             edge_index_list.append(e + max_index)  # Add the maximum index to adjust the indices
             max_index += x_padded.size(1)  # Update the maximum index
         edge_index_padded = torch.cat(edge_index_list, dim=1)
-        edge_attr_padded = edge_attr_padded.reshape(edge_index_padded.size(1), 1)  # Reshape to (num_edges, 1)
         num_nodes = max_index + 1  # Update the number of nodes based on the adjusted indices
         adj_matrix = torch.zeros(num_nodes, num_nodes, dtype=torch.float32)  # Adjust the size of adj_matrix
         for i, j in edge_index_padded.t():
             adj_matrix[i, j] = 1.0
-        output = model(x_padded, edge_index_padded, edge_attr_padded)  # Update the model forward call
+        output = model(x_padded, edge_index_padded)  # Update the model forward call
         predicted_labels = output.argmax(dim=1)
         total_correct += (predicted_labels == y).sum().item()
         total_samples += y.size(0)
