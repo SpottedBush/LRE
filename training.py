e7fa1f8038fdaed5f1254d17f01a029552060008
SpottedBush <vincenttardieux22@gmail.com>
2023-06-01 00:38:58 +0200

    fix(training.py): changed the reshape to get the actual number of edges

--- training.py
+++ training.py
@@ -103,7 +103,7 @@ for epoch in range(num_epochs):
             edge_index_list.append(e + max_index)  # Add the maximum index to adjust the indices
             max_index += x_padded.size(1)  # Update the maximum index
         edge_index_padded = torch.cat(edge_index_list, dim=1)
-        edge_attr_padded = edge_attr_padded.reshape(-1, 1)  # Reshape to (num_edges, 1)
+        edge_attr_padded = edge_attr_padded.reshape(edge_index.size(1), 1)  # Reshape to (num_edges, 1)
         num_nodes = max_index + 1  # Update the number of nodes based on the adjusted indices
         adj_matrix = torch.zeros(num_nodes, num_nodes, dtype=torch.float32)  # Adjust the size of adj_matrix
         for i, j in edge_index_padded.t():
@@ -133,7 +133,7 @@ with torch.no_grad():
             edge_index_list.append(e + max_index)  # Add the maximum index to adjust the indices
             max_index += x_padded.size(1)  # Update the maximum index
         edge_index_padded = torch.cat(edge_index_list, dim=1)
-        edge_attr_padded = edge_attr_padded.reshape(-1, 1)  # Reshape to (num_edges, 1)
+        edge_attr_padded = edge_attr_padded.reshape(edge_index.size(1), 1)  # Reshape to (num_edges, 1)
         num_nodes = max_index + 1  # Update the number of nodes based on the adjusted indices
         adj_matrix = torch.zeros(num_nodes, num_nodes, dtype=torch.float32)  # Adjust the size of adj_matrix
         for i, j in edge_index_padded.t():
