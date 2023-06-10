f8aab6f1405ca1ac5daba264e0bc6453b5e5d93f
SpottedBush <vincenttardieux22@gmail.com>
2023-06-10 13:50:45 +0200

    fix(training.py): changed the batch size and added the graph plot

--- training.py
+++ training.py
@@ -6,22 +6,47 @@ from torch.optim import Adam
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from torch_geometric.data import Data, Batch
+from matplotlib.pylab import plt
+from numpy import arange
 
 from fen_into_graphs import fen_into_graph
 
 import os
 
-BATCH_SIZE = 130
+BATCH_SIZE = 260
 
 def matprint(mat):
+    print("conf_matrix is: ")
     for column in mat:
+        print("|  ", end = "")
         for line in column:
             print(line, end=" | ")
         print("")
+    print("\n")
             
 
 mat = [[1,2,3],[4,5,6],[7,8,9]]
 matprint(mat)
+
+def plt_print(train_values):
+    # Generate a sequence of integers to represent the epoch numbers
+    epochs = range(len(train_values))
+     
+    # Plot and label the training and validation loss values
+    plt.plot(epochs, train_values, label='Training Loss')
+    #plt.plot(epochs, val_values, label='Validation Loss')
+     
+    # Add in a title and axes labels
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+     
+    # Set the tick locations
+    plt.xticks(arange(0, len(train_values), 2))
+     
+    # Display the plot
+    plt.legend(loc='best')
+    plt.show()
 
 class ChessGNN(nn.Module):
     def __init__(self, num_node_features, hidden_channels, num_classes):
@@ -92,10 +117,10 @@ iterator = iter(graph_train_loader)
 batch = next(iterator)
 
 
-print("------ Finished reading file, starting the training ------")
+print("\n------ Finished reading file, starting the training ------\n\n")
 num_nodes = 65
 num_node_features = 7  # Number of node features (piece and team)
-hidden_channels = 16  # Number of hidden channels in GNN layers
+hidden_channels = 64  # Number of hidden channels in GNN layers
 num_classes = 5  # Number of classes for graph classification
 model = ChessGNN(num_node_features, hidden_channels, num_classes)
 
@@ -104,15 +129,16 @@ optimizer = Adam(model.parameters(), lr=0.01)
 
 # Loss function
 criterion = nn.NLLLoss()
+#criterion = nn.CrossEntropyLoss()
 
 # Training the model
 model.train()
-f = open(os.path.join('trained_models', 'results2.txt'), "w+")
+f = open(os.path.join('trained_models', 'results3.txt'), "w+")
 num_epochs = 100
+training_values = []
 for epoch in range(num_epochs):
     total_loss = 0
     total_samples = 0
-    count = 0
     conf_matrix = [[0 for x in range(len(categories))] for x in range(len(categories))]
     for elem in graph_train_loader:
         for x, edge_index, y, num_node_features, num_nodes in elem:
@@ -131,21 +157,20 @@ for epoch in range(num_epochs):
             loss = criterion(output, y)
             predicted = torch.max(output.data, 1).indices
             predicted_label = round(predicted.float().mean().item())
-            # if count == 0 or count == 3:
-            #     print("actual: ", actual_label)
-            #     print("predicted: ", predicted)
-            #     print("predicted_label: ", predicted_label)
+           
             conf_matrix[actual_label][predicted_label] += 1
             loss.backward()
             optimizer.step()
             total_loss += loss.item() * y.size(0)
             total_samples += y.size(0)
-            count += 1
+    training_values.append(total_loss)
     avg_loss = total_loss / total_samples
     f.write(f"Epoch {epoch + 1} - Loss: {avg_loss}\n")
     print(f"Epoch {epoch + 1} - Loss: {avg_loss}\n")
+    matprint(conf_matrix)
+    
+plt_print(training_values)
 
-matprint(conf_matrix)
 print("\n------ Finished training, starting the evaluation ------\n\n")
 
 # Evaluating the model
@@ -170,6 +195,6 @@ with torch.no_grad():
     print(f"Accuracy: {accuracy}\n")
 
 # Save the model
-save_path = os.path.join('trained_models', 'trained_model2.pt')
+save_path = os.path.join('trained_models', 'trained_model3.pt')
 torch.save(model.state_dict(), save_path)
 print(f"Model saved at {save_path}")
